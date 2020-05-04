4e1ffd8d2188adda1e41207e63c61906ed27bc07
Ricardo Dias <ricardodc@gmail.com>
2020-05-04 09:12:10 -0300

    [WIP] Camera working

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,7 +2,7 @@ import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import React from "react";
 import BarCodeAppBar from "./BarCodeAppBar";
 import BarcodeCardList from "./BarcodeCardList";
-import BarcodeStartScanButton from "./BarcodeStartScanButton";
+import BarcodeScanner from "./BarcodeScanner";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -12,17 +12,13 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-export const openScanner = () => {
-  console.log("Teste");
-};
-
 const App = () => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
       <BarCodeAppBar />
       <BarcodeCardList />
-      <BarcodeStartScanButton openScanner={openScanner} />
+      <BarcodeScanner />
     </div>
   );
 };
