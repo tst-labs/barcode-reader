0a61ee2706f193121a724ba2e7dc5e32fb33928e
Ricardo Dias <ricardodc@gmail.com>
2020-05-04 10:19:17 -0300

    Updates

---
+++ precache-manifest.af2135776698f41be848393cc73934b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b835bbe37e0b6209fadc336be41dbec",
+    "url": "/barcode-reader/index.html"
+  },
+  {
+    "revision": "a3e0f389f4c9f6dd7abe",
+    "url": "/barcode-reader/static/js/2.180ad659.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/barcode-reader/static/js/2.180ad659.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "607afbf20a94a21dbc4d",
+    "url": "/barcode-reader/static/js/main.4f2b3971.chunk.js"
+  },
+  {
+    "revision": "8738bdc5a8bf12392249",
+    "url": "/barcode-reader/static/js/runtime-main.faa5c9b7.js"
+  },
+  {
+    "revision": "2183cd00945ec1893cecea950a45b0ef",
+    "url": "/barcode-reader/static/media/barcode.2183cd00.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8ae0a839f4d8c5945f660c8335003c6d.js"
+  "/barcode-reader/precache-manifest.af2135776698f41be848393cc73934b4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/barcode-reader/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
